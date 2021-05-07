82fbe7015f02b3c357f0025bcec3409dbc25cdde
samruddhi-sj <71511598+samruddhi-sj@users.noreply.github.com>
2021-05-07 20:14:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,10 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-
+let port = process.env.PORT;
+if(port==NULL || port="")
+  port=3000;
+app.listen(port);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
