9c09a4501c1294b3ccdb8919ffc7ceed0d77a7e1
samruddhi-sj <samruddhi.jadhav18@vit.edu>
2021-06-19 20:13:37 +0530

    Second commit

--- app.js
+++ app.js
@@ -74,11 +74,8 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
-// let port = process.env.PORT;
-// if(port==null || port=="")
-//   port=3000;
-// app.listen(port);
+var port = process.env.PORT || 8080;
 
-app.listen(3000, function() { 
-  console.log("Server started on port 3000");
+app.listen(port, function() { 
+  console.log("Server started");
 });
